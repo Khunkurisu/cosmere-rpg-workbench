52a8d606869a75b30d2658f549b7eaf49a65e90b
Khunkurisu <creative.3d@live.com>
2025-07-28 09:31:16 -0500

    feat: simple constants file for reference in other areas
    Signed-off-by: Khunkurisu <creative.3d@live.com>

---
+++ src/module/constants/index.ts
@@ -0,0 +1,19 @@
+/**
+ * String identifier for the module used throughout other scripts.
+ */
+export const MODULE_ID = 'cosmere-rpg-workbench';
+
+/**
+ * Full title string of the module.
+ */
+export const MODULE_NAME = 'Cosmere RPG Workbench';
+
+/**
+ * String identifier for the system used throughout other scripts.
+ */
+export const SYSTEM_ID = 'cosmere-rpg';
+
+/**
+ * Author name of the module.
+ */
+export const AUTHOR_NAME = 'Khunkurisu';
