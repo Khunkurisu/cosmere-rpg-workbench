7607090beaa3ae0082fa23afeed9629db33b6158
Khunkurisu <creative.3d@live.com>
2025-07-28 09:20:22 -0500

    chore: fix scripts and node packages
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- scripts/link-foundry.js
+++ scripts/link-foundry.js
@@ -5,7 +5,9 @@ import prompts from 'prompts';
 
 const BUILD_OUTPUT_FOLDER = 'build';
 const FOUNDRY_DATA_PATH_VALIDATION_REGEX = /Data$/; // Does the path end in "Data"
-const FOUNDRY_EXPECTED_SUBFOLDERS = ['modules', 'systems', 'worlds'];
+const FOUNDRY_EXPECTED_SUBFOLDERS = [
+    'modules', 'systems', 'worlds'
+];
 
 // Check if OS is windows
 const isWin32 = process.platform === 'win32';
@@ -15,9 +17,7 @@ const buildFolderPath = path.join(process.cwd(), BUILD_OUTPUT_FOLDER);
 
 // Ensure build folder exists (current working directory is in project root)
 if (!folderExistsAtPath(buildFolderPath)) {
-    console.error(
-        `Could not find build folder. Are you at the root of the project? Make sure to run the build once before linking.`,
-    );
+    console.error(`Could not find build folder. Are you at the root of the project? Make sure to run the build once before linking.`);
     process.exit(1);
 }
 
@@ -28,52 +28,47 @@ const { dataPath } = await prompts({
     message: 'Enter the path to your Foundry data folder.',
     validate: (value) => {
         if (!FOUNDRY_DATA_PATH_VALIDATION_REGEX.test(value)) {
-            return `"${value}" does not look like a valid Foundry data path. Make sure the path ends in "${isWin32 ? '\\' : '/'}Data".`;
-        }
+            return `"${value}" does not look like a valid Foundry data path. Make sure the path ends in "${isWin32 ? '\\' : '/'}Data".`
+        } 
 
         // Resolve the path
         const absolutePath = path.resolve(value);
 
         // Ensure path exists
         if (!folderExistsAtPath(absolutePath)) {
-            return `No folder found at "${absolutePath}".`;
+            return `No folder found at "${absolutePath}".`
         }
 
         // Path is valid, but is it Foundry? (check if expected subfolders exist)
         const allSubfoldersExist = FOUNDRY_EXPECTED_SUBFOLDERS.every(
-            (subfolder) =>
-                folderExistsAtPath(path.join(absolutePath, subfolder)),
+            subfolder => folderExistsAtPath(path.join(absolutePath, subfolder)) 
         );
         if (!allSubfoldersExist) {
             return `"${absolutePath}" does not look like a valid Foundry data path.`;
         }
 
         return true;
-    },
+    }
 });
 
-// Construct path to symlink
-const symlinkPath = path.resolve(dataPath, 'systems', 'cosmere-rpg');
+// Construct path to symlink 
+const symlinkPath = path.resolve(dataPath, 'modules', 'cosmere-rpg-stormlight-handbook');
 
 const stats = fs.statSync(symlinkPath, { throwIfNoEntry: false });
 if (stats) {
-    const objectAtPath = stats.isFile()
-        ? 'file'
-        : stats.isDirectory()
-          ? 'folder'
-          : stats.isSymbolicLink()
-            ? 'symlink'
-            : '<unknown>';
+    const objectAtPath = stats.isFile() ? 'file' :
+        stats.isDirectory() ? 'folder' :
+        stats.isSymbolicLink() ? 'symlink' : '<unknown>';
 
     const { shouldProceed } = await prompts({
         type: 'confirm',
         name: 'shouldProceed',
         initial: false,
-        message: `A ${objectAtPath} already exists at "${symlinkPath}". Replace with new symlink?`,
+        message: `A ${objectAtPath} already exists at "${symlinkPath}". Replace with new symlink?`
     });
 
     if (!shouldProceed) {
-        console.log('Aborting.');
+        console.log('Aborting.')
         process.exit();
     }
 
@@ -88,15 +83,11 @@ if (stats) {
 try {
     fs.symlinkSync(buildFolderPath, symlinkPath);
 } catch (err) {
-    console.error(
-        `An unexpected error occured while trying to create a symlink: ${err.message ?? ''}`,
-    );
+    console.error(`An unexpected error occured while trying to create a symlink: ${err.message ?? ''}`);
     process.exit(1);
 }
 
-console.log(
-    `Successfully created a symlink between ${buildFolderPath} <==> ${symlinkPath}.`,
-);
+console.log(`Successfully created a symlink between ${buildFolderPath} <==> ${symlinkPath}.`);
 
 /* --- Helpers --- */
 
