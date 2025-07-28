1dcd89083f6a7a96480a7761dcbfd0007300893e
Khunkurisu <creative.3d@live.com>
2025-07-28 10:16:37 -0500

    feat: remove unused junk feat: call new settings registry function
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import './module/cosmere-rpg-api';
 import { register } from './module/register';
 import { COSMERE_WORKBENCH } from './module/helpers/config.mjs';
 import { preloadHandlebarsTemplates } from './module/helpers/templates.mjs';
-import { registerSettings } from './module/helpers/register-settings.mjs';
+import { registerModuleSettings } from './module/settings';
 
 declare global {
 	interface LenientGlobalVariableTypes {
@@ -21,29 +21,15 @@ declare global {
 	}
 };
 
-let registeredSkills: Array<CosmereAPI.SkillConfigData>;
-let registeredCurrency: Array<CosmereAPI.CurrencyConfigData>;
-
 Hooks.once('init', async function () {
 	/* globalThis.cosmereWorkbench = Object.assign(
 		{ macros: WorkbenchMacros }
 	); */
 	CONFIG.COSMERE_WORKBENCH = COSMERE_WORKBENCH;
-	registerSettings();
+	registerModuleSettings();
 
 	register();
 
 	// Preload Handlebars templates.
 	return preloadHandlebarsTemplates();
 });
-
-/* Hooks.once('ready', () => {
-	LocalizeSkills(registeredSkills);
-	LocalizeCurrency(registeredCurrency);
-});
-
-Hooks.on('renderActorSheetV2', async (o, i, n) => {
-	await InjectCurrencyCounter(o, i);
-	await InjectEncumbranceCounter(o, i);
-	return true;
-}); */
