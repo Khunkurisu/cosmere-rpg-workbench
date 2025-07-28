f079af786ec9371077cd94357a1c7de34efbe961
Khunkurisu <creative.3d@live.com>
2025-07-28 09:23:36 -0500

    chore: delete old unused applications
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- src/module/macros/manage-talents.mjs
+++
@@ -1,6 +0,0 @@
-import { TalentManagerMenu } from "../applications/talent-manager-menu.mjs"
-
-export default async function TalentManager() {
-	const talentManager = new TalentManagerMenu();
-	await talentManager.render({ force: true });
-}
