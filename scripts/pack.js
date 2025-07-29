82ecc2edb455a83d93afbcd388d1bfef6545dcf9
Khunkurisu <creative.3d@live.com>
2025-07-29 04:16:06 -0500

    fix: if can't make new folder return early on pack()
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- scripts/pack.js
+++ scripts/pack.js
@@ -8,32 +8,33 @@ const PACK_DEST = path.join('build', 'packs');
 
 async function compilePacks() {
 	if (!fs.existsSync(PACK_DEST)) { fs.mkdirSync(PACK_DEST); }
-    const folders = fs.readdirSync(PACK_SRC, { withFileTypes: true })
-        .filter(file => file.isDirectory());
+	if (!fs.existsSync(PACK_DEST)) { return; }
+	const folders = fs.readdirSync(PACK_SRC, { withFileTypes: true })
+		.filter(file => file.isDirectory());
 
-    for (const folder of folders) {
-        const src = path.join(PACK_SRC, folder.name);
-        const dest = path.join(PACK_DEST, folder.name);
+	for (const folder of folders) {
+		const src = path.join(PACK_SRC, folder.name);
+		const dest = path.join(PACK_DEST, folder.name);
 
-        console.log(`Compiling pack ${folder.name}`);
-        await compilePack(src, dest, { recursive: true, log: true });
-    }
+		console.log(`Compiling pack ${folder.name}`);
+		await compilePack(src, dest, { recursive: true, log: true });
+	}
 }
 
 function cleanPackEntry(data, { clearSourceId = true, ownership = 0 } = {}) {
-    if (data.ownership) data.ownership = { default: ownership };
-    if (clearSourceId) {
-        delete data._stats?.compendiumSource;
-        delete data.flags?.core?.sourceId;
-    }
-    delete data.flags?.importSource;
-    delete data.flags?.exportSource;
+	if (data.ownership) data.ownership = { default: ownership };
+	if (clearSourceId) {
+		delete data._stats?.compendiumSource;
+		delete data.flags?.core?.sourceId;
+	}
+	delete data.flags?.importSource;
+	delete data.flags?.exportSource;
 
-    // Remove empty entries in flags
-    if ( !data.flags ) data.flags = {};
-    Object.entries(data.flags).forEach(([key, contents]) => {
-        if ( Object.keys(contents).length === 0 ) delete data.flags[key];
-    });
+	// Remove empty entries in flags
+	if (!data.flags) data.flags = {};
+	Object.entries(data.flags).forEach(([key, contents]) => {
+		if (Object.keys(contents).length === 0) delete data.flags[key];
+	});
 }
 
 await compilePacks();
