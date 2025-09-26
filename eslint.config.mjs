2aa84aea1a4c878da1482961cba91d4b71c44f71
Khunkurisu <creative.3d@live.com>
2025-09-26 09:17:41 -0500

    chore: allow explicit any in tseslint
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,8 @@ export default tseslint.config(
             '@typescript-eslint/no-unsafe-argument': 'off',
             '@typescript-eslint/no-unsafe-return': 'off',
             '@typescript-eslint/no-empty-function': 'off',
-            '@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
             'no-unexpected-multiline': 'off',
         },
         languageOptions: {
