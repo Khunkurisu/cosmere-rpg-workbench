a61edda7ca2c4f0b8d6616b7e54d967e859c9446
Khunkurisu <creative.3d@live.com>
2025-09-26 09:50:40 -0500

    fix: proper export reference
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- src/module/applications/index.ts
+++ src/module/applications/index.ts
@@ -1,3 +1,4 @@
 export { CustomSkillMenuV2 as CustomSkillMenu } from './custom-skills-menu';
 export { CustomCurrencyMenuV2 as CustomCurrencyMenu } from './custom-currency-menu';
-export { CompendiumBrowser } from "./compendium-browser";
+export { CompendiumBrowser } from "./compendium-browser/compendium-browser";
+export { CompendiumManager } from "./compendium-browser/compendium-manager";
